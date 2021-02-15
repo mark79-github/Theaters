6513ad8bc7b78e9858bddb6faa6d2bd5eb2c42bd
Martin Dimitrov <martin_dimitrov@abv.bg>
2021-02-15 16:25:18 +0200

    added task conditions and resources

--- config/config.js
+++ config/config.js
@@ -1,16 +1,17 @@
-const {username, password, db, privateKey} = require('./credentials.js');
+const credentials = require('./credentials.js');
+const {server, db, username, password, privateKey} = credentials;
 
 const config = {
     development: {
         PORT: 3000,
-        DB: `mongodb://127.0.0.1/${db}`,
+        DB: `mongodb://${server}/${db}`,
         saltRounds: 10,
         authCookie: 'jwt-auth-cookie',
         privateKey,
     },
     production: {
         PORT: 80,
-        DB: `mongodb+srv://${username}:${password}@cluster0.nbulj.mongodb.net/${db}?retryWrites=true&w=majority`,
+        DB: `mongodb+srv://${username}:${password}@${server}/${db}?retryWrites=true&w=majority`,
         saltRounds: 7,
         authCookie: 'jwt-auth-cookie',
         privateKey,
